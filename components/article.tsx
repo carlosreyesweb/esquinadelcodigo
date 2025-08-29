d1f791ef2cd6a5bb7b96e4b38ff6ef394fc4dfc6
carlosreyesweb <contact@carlosreyesweb.com>
2025-08-28 21:07:17 -0400

    hotfix: fix article relative date parsing

--- components/article.tsx
+++ components/article.tsx
@@ -11,41 +11,42 @@ interface ArticleProps {
 }
 export function Article({ data }: ArticleProps) {
   const link = `/${encodeURIComponent(data.slug)}`
-  const createdAt = formatRelative(new Date(data.createdAt), new Date(), {
+  const createdAt = formatRelative(data.createdAt, new Date(), {
     locale: es,
   })
   const { readingTimeString } = useReadingTime(data.content)
 
   return (
-    <Link
-      href={link}
-      className="group block ring-primary hover:ring-2 focus:outline-none focus:ring-2"
-    >
-      <article className="space-y-3 border bg-card px-5 py-4">
-        <Typography variant="h3" className="text-primary">
-          {data.title}
-        </Typography>
-        <Typography variant="p" className="line-clamp-2">
-          {data.teaser}
-        </Typography>
-        <Typography variant="smallText" className="block text-muted-foreground">
-          <time dateTime={data.createdAt}>{createdAt}</time>
-          {" — "}
-          <time>🕜 {readingTimeString}</time>
-        </Typography>
-        <ul role="list" className="flex flex-wrap gap-2">
-          {data.tags.map((tag) => (
-            <Badge
-              key={tag}
-              asChild
-              variant="outline"
-              className="border-primary text-primary"
-            >
-              <li>{tag}</li>
-            </Badge>
-          ))}
-        </ul>
-      </article>
-    </Link>
+    <article className="group relative space-y-3 border bg-card px-5 py-4 ring-primary focus-within:ring-2 hover:ring-2">
+      <Typography variant="h3" className="text-primary">
+        {data.title}
+      </Typography>
+      <Typography variant="p" className="line-clamp-2">
+        {data.teaser}
+      </Typography>
+      <Typography variant="smallText" className="block text-muted-foreground">
+        <time dateTime={data.createdAt}>{createdAt}</time>
+        {" — "}
+        <time>🕜 {readingTimeString}</time>
+      </Typography>
+      <ul role="list" className="flex flex-wrap gap-2">
+        {data.tags.map((tag) => (
+          <Badge
+            key={tag}
+            asChild
+            variant="outline"
+            className="border-primary text-primary"
+          >
+            <li>{tag}</li>
+          </Badge>
+        ))}
+      </ul>
+      <Link
+        href={link}
+        className="group absolute inset-0 block select-none focus:outline-none"
+      >
+        <span className="sr-only">Leer &quot;{data.title}&quot;</span>
+      </Link>
+    </article>
   )
 }
